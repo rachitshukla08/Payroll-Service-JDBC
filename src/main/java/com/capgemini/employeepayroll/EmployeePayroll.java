84d028020d7ccf7a407a299220b17ba9c8dbe141
unknown <rachitshukla08@gmail.com>
2020-10-28 13:49:51 +0530

    Added: MySQL connector dependency to project and established a connection to MySQL

---
+++ src/main/java/com/capgemini/employeepayroll/EmployeePayroll.java
@@ -0,0 +1,45 @@
+package com.capgemini.employeepayroll;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+/**
+ * @author Rachit 
+ * JDBC
+ */
+public class EmployeePayroll {
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "root";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find driver in classpath");
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database: " + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection successful: " + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * List the drivers being loaded
+	 */
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("   " + driverClass.getClass().getName());
+		}
+	}
+}
